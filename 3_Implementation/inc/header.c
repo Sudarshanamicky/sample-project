005d697e2355994b6d430943764d26ba2cc46879
99002501 <71481755+99002501@users.noreply.github.com>
2020-09-19 10:10:04 +0530

    Create header.c

---
+++ 3_Implementation/inc/header.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+  void login();
+  void insert_details();
+  void view_All();
+  void find();
+  void book_ticket();
+  void old_records();
